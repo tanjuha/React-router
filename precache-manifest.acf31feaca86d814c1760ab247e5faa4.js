25893cadc060a48be6fa5951e921b0d76b2ac98c
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-05-19 17:34:32 +0300

    Updates

---
+++ precache-manifest.acf31feaca86d814c1760ab247e5faa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d105e8b55f779c095e14efac619a7873",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "236495463b4e9e7a2f3c",
+    "url": "/react-router/static/css/main.5b690ff4.chunk.css"
+  },
+  {
+    "revision": "fd24eede7bb792740745",
+    "url": "/react-router/static/js/2.a51082a8.chunk.js"
+  },
+  {
+    "revision": "236495463b4e9e7a2f3c",
+    "url": "/react-router/static/js/main.93fa0001.chunk.js"
+  },
+  {
+    "revision": "c6984152ca93223c2cb4",
+    "url": "/react-router/static/js/runtime~main.fdc07815.js"
+  }
+]);
